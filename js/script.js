ae2b917081c583a1091bc633b5925481385da430
Adesayo Habeeb <adesayo4@gmail.com>
2025-11-22 12:26:56 +0100

    Changes made

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+// --- 1. Active Navigation Link ---
+// This code highlights the current page in the navigation bar.
+const navLinks = document.querySelectorAll('nav a');
+const currentPage = window.location.pathname.split('/').pop(); // Gets the current file name (e.g., "about.html")
+
 alert('Welcome to Nova Fashion House!');
